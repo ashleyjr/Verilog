c8b196cd1e8c7fd5f8f5a88440fe5f013a6a7c0d
Ashley J. Robinson <ashley181291@gmail.com>
2015-06-23 12:13:26 +0100

    update runsim

--- runsim.py
+++ runsim.py
@@ -8,9 +8,6 @@ from optparse import OptionParser
 
 
 if "__main__" == __name__:
-    print
-    print "--- MLH runsim ---"
-
     parser = OptionParser(usage="runsim.py [-m module] [-s do a sim] [-w view waves]" )
     parser.add_option("-m", "--module", dest="module", help="module to simulate - should not be defined if program is")
     parser.add_option("-s", "--sim", action="store_true", dest="sim")
@@ -21,28 +18,33 @@ if "__main__" == __name__:
 
     move = "cd " + sim
     os.chdir(sim)
-    print "MOVE: " + move + "/"
+
+    print
+    print "--- runsim.py ---"
+    print "  Module: "+ sim
+    print "    Move: " + move + "/"
 
     if((options.sim == None) and (options.waves == None)):
-        print "INFO: You must specify and operation"
+        print "    Info: You must specify and operation"
 
 
     if(options.sim):
-        print "INFO: Simulate " + str(sim)
+        print "    Info: Simulate " + str(sim)
 
         cmd = "iverilog -o " + str(sim) + ".dat -c " + str(sim) +"_filelist.txt"
-        print " CMD: " + cmd
+        print "     Cmd: " + cmd
         os.system(cmd)
 
 
         cmd = "vvp " + str(sim) + ".dat -vcd"
-        print " CMD: " + cmd
+        print "     Cmd: " + cmd
         os.system(cmd)
 
 
     if(options.waves):
         cmd = "gtkwave " + str(sim) +".vcd"
-        print " CMD: " + cmd
+        print "     Cmd: " + cmd
         os.system(cmd)
 
+    print "DONE"
     print
