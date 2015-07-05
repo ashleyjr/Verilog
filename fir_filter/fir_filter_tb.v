e6b146329393c139718fd64b2b1c323fa4b5e083
Ashley J. Robinson <ashley181291@gmail.com>
2015-07-05 01:09:58 +0100

    Pass through on filter

--- fir_filter/fir_filter_tb.v
+++ fir_filter/fir_filter_tb.v
@@ -6,31 +6,14 @@ module fir_filter_tb;
 
    reg            clk;
    reg            nRst;
-   reg            tx;
-   wire           rx;
-   wire           recieved;
-   wire           busy_rx;
-   wire           busy_tx;
-   wire  [7:0]    data_tx;
-   wire  [7:0]    data_rx;
-
-   integer i,j;
-
-   reg sample_rx;
-   reg sample_tx;
-
-
+   reg   [31:0]   in;
+   wire  [31:0]   out;
+      
    fir_filter fir_filter(
       .clk        (clk        ),
       .nRst       (nRst       ),
-      .transmit   (recieved   ),
-      .data_tx    (data_rx    ),
-      .rx         (tx         ),
-      .busy_rx    (busy_rx    ),
-      .busy_tx    (busy_tx    ),
-      .recieved   (recieved   ),
-      .data_rx    (data_rx    ),
-      .tx         (rx         )
+      .in         (in         ),
+      .out        (out        )
    );
 
 	initial begin
@@ -44,87 +27,14 @@ module fir_filter_tb;
       $dumpfile("fir_filter.vcd");
       $dumpvars(0,fir_filter_tb);
    end
-
-   task uart_send_1;
-      input [7:0] send;
-      integer i;
-      begin
-         tx = 0;
-         for(i=0;i<=7;i=i+1) begin   
-            sample_tx = !sample_tx;
-            #BAUD_PERIOD_1 tx = send[i];
-         end
-         sample_tx = !sample_tx;
-         #BAUD_PERIOD_1 tx = 1;
-      end
-   endtask
-
-   task uart_send_2;
-      input [7:0] send;
-      integer i;
-      begin
-         tx = 0;
-         for(i=0;i<=7;i=i+1) begin   
-            sample_tx = !sample_tx;
-            #BAUD_PERIOD_2 tx = send[i];
-         end
-         sample_tx = !sample_tx;
-         #BAUD_PERIOD_2 tx = 1;
-      end
-   endtask
-
-
 	
    initial begin
-               sample_tx = 0;
-               sample_rx = 0;
-      #100     nRst = 1;
-               tx = 1;
-      #100     nRst = 0;
-      #50     nRst = 1;
-
-      //for(i=1000;i>0;i=i-1) begin
-      //   for(j=0;j<i;j=j+1) begin
-      //      #1 tx = 1;
-      //   end
-      //
-      //for(j=0;j<i;j=j+1) begin
-      //      #1 tx = 0;
-      //   end
-      //end
-      #100000
-      uart_send_1(8'h11);
-      #100000
-      uart_send_1(8'hAA);
-      #100000
-      uart_send_1(8'h11);
-      #100000
-      uart_send_1(8'hAA);
-
-
-      #100000
-      uart_send_2(8'h11);
-      #100000
-      uart_send_2(8'hAA);
-      #100000
-      uart_send_2(8'h11);
-      #100000
-      uart_send_2(8'hAA);
-
-      
-      #100000
-      uart_send_1(8'h11);
-      #100000
-      uart_send_1(8'hAA);
-      #100000
-      uart_send_1(8'h11);
-      #100000
-      uart_send_1(8'hAA);
-
-
-
-
-      #300000
+               in    = 32'd100;
+      #100     nRst  = 1;
+      #100     nRst  = 0;
+      #50      nRst  = 1;
+      #300     in    = 32'd101;
+      #300     in    = 32'd102;
 	   $finish;
 	end
 
