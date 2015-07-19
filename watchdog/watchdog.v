18d946ba33b495c20f094e8bc0b907ff84928de3
Ashley J. Robinson <ashley181291@gmail.com>
2015-07-19 01:31:34 +0100

    asynf ruins watch dog?

--- watchdog/watchdog.v
+++ watchdog/watchdog.v
@@ -1,21 +1,31 @@
 module watchdog(
 	input	   clk,
 	input	   nRst,
-   input    sleep,
-   output   woof
+   input    sleep,   // Reset the watchdog
+   input    sclk,    // serial clock
+   input    in,      // serial data in
+   input    sel,     // serial input listening when high
+   output   woof     // The counter has lapsed when high
 );
 
-   reg [31:0] count;
+   reg   [31:0]   top;
+   reg   [31:0]   count;
 
    assign woof = (count) ? 1'b0 : 1'b1;
 
    always @(posedge clk or negedge nRst) begin
       if(!nRst | sleep) begin
-         count <= 32'd100;
+         count <= top;
       end else begin
          if(count) begin
             count <= count - 32'd1;
          end 
+      end
+   end
+
+   always @(posedge sclk) begin
+      if(sel) begin
+         top <= {top[31:1],in};
       end
    end
 endmodule
--- watchdog/watchdog_tb.v
+++ watchdog/watchdog_tb.v
@@ -5,12 +5,18 @@ module watchdog_tb;
 	reg   clk;
 	reg   nRst;
    reg   sleep;
+   reg   sclk;
+   reg   in;
+   reg   sel;
    wire  woof;
 
 	watchdog watchdog(
 		.clk	   (clk),
 		.nRst	   (nRst),
       .sleep   (sleep),
+      .sclk    (sclk),
+      .in      (in),
+      .sel     (sel),
       .woof    (woof)
 	);
 
@@ -26,11 +32,26 @@ module watchdog_tb;
 	end
 
    initial begin
-            nRst = 1;
+      while(1) begin
+         #100  sclk = 0;
+         #100  sclk = 1;
+      end
+   end
+   initial begin
+            nRst  = 1;
             sleep = 0;
+            in    = 0;
+      #100  nRst  = 0;
+      #100  nRst  = 1;
+      #100  sel   = 1;
+      #400  in    = 1;
+      #100  in    = 0;
+      #20   sel   = 0;
+      
+      #100  nRst = 1;
       #100  nRst = 0;
       #100  nRst = 1;
-      #100  sleep = 1;
+      
       #3000
       $finish;
    end
