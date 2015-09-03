dc52aae132cd274df97cbf6a2fcbd4900ac5a2d9
Ashley J. Robinson <ashley181291@gmail.com>
2015-09-03 21:41:59 +0100

    started writing verilog

--- up/up_tb.v
+++ up/up_tb.v
@@ -2,13 +2,26 @@ module up_tb;
 
 	parameter CLK_PERIOD = 20;
 
-	reg clk;
-	reg nRst;
+	reg            clk;
+	reg            nRst;
+   reg   [1:0]    sel_out_a;
+   reg   [1:0]    sel_out_b;
+   reg   [2:0]    sel_write;
+   reg   [7:0]    data_in;
+   wire  [7:0]    data_out_a;
+   wire  [7:0]    data_out_b;
+
 
 	up up(
-		.clk	(clk),
-		.nRst	(nRst)
-	);
+		.clk	      (clk        ),
+		.nRst	      (nRst       ),
+	   .sel_out_a  (sel_out_a  ),
+      .sel_out_b  (sel_out_b  ),
+      .sel_write  (sel_write  ),
+      .data_in    (data_in    ),
+      .data_out_a (data_out_a ),
+      .data_out_b (data_out_b ) 
+   );
 
 	initial begin
 		while(1) begin
