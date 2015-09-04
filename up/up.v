f340dbeb4c38d72852ffe1eab565d27dbaa92a43
Ashley Robinson <ashley181291@gmail.com>
2015-09-04 16:02:52 +0100

    edit without sim

--- up/up.v
+++ up/up.v
@@ -19,15 +19,10 @@ module up(
                REG_ON_RES_2   = 8'h03,
                REG_ON_RES_3   = 8'h04;
 
-   parameter   SEL_WRITE_0    = 2'b00,
-               SEL_WRITE_1    = 2'b01,
-               SEL_WRITE_2    = 2'b10,
-               SEL_WRITE_3    = 2'b11;
-
-   parameter   SEL_OUT_0      = 2'b00,
-               SEL_OUT_1      = 2'b01,
-               SEL_OUT_2      = 2'b10,
-               SEL_OUT_3      = 2'b11;
+   parameter   SEL_0          = 2'b00,
+               SEL_1          = 2'b01,
+               SEL_2          = 2'b10,
+               SEL_3          = 2'b11;
 
    reg [7:0]   r0;
    reg [7:0]   r1;
@@ -35,14 +30,14 @@ module up(
    reg [7:0]   r3;   
 
    assign   data_out_a = 
-               (sel_out_a == SEL_OUT_0) ? r0:
-               (sel_out_a == SEL_OUT_1) ? r1:
-               (sel_out_a == SEL_OUT_2) ? r2: r3;
+               (sel_out_a == SEL_0) ? r0:
+               (sel_out_a == SEL_1) ? r1:
+               (sel_out_a == SEL_2) ? r2: r3;
 
    assign   data_out_b = 
-               (sel_out_b == SEL_OUT_0) ? r0:
-               (sel_out_b == SEL_OUT_1) ? r1:
-               (sel_out_b == SEL_OUT_2) ? r2: r3;
+               (sel_out_b == SEL_0) ? r0:
+               (sel_out_b == SEL_1) ? r1:
+               (sel_out_b == SEL_2) ? r2: r3;
 
  
    always@(posedge clk or negedge nRst) begin 
@@ -52,18 +47,45 @@ module up(
          r2 <= REG_ON_RES_2;
          r3 <= REG_ON_RES_3;
       end else begin
-         if(we_a) case(sel_write_a) 
-                     SEL_WRITE_0:   r0 <= data_in_a;
-                     SEL_WRITE_1:   r1 <= data_in_a;
-                     SEL_WRITE_2:   r2 <= data_in_a;
-                     SEL_WRITE_3:   r3 <= data_in_a;
-                  endcase
-         if(we_b) case(sel_write_b) 
-                     SEL_WRITE_0:   r0 <= data_in_b;
-                     SEL_WRITE_1:   r1 <= data_in_b;
-                     SEL_WRITE_2:   r2 <= data_in_b;
-                     SEL_WRITE_3:   r3 <= data_in_b;
-                  endcase
+         if(we_a) 
+            case(sel_write_a) 
+               SEL_0:   begin
+                           r0 <= data_in_a;
+                           if(we_b)
+                              case(sel_write_b)
+                                 SEL_1:   r1 <= data_in_b;
+                                 SEL_2:   r2 <= data_in_b;
+                                 SEL_3:   r3 <= data_in_b;
+                              endcase
+                        end    
+               SEL_1:   begin
+                           r1 <= data_in_a;
+                           if(we_b)
+                              case(sel_write_b)
+                                 SEL_0:   r0 <= data_in_b;
+                                 SEL_2:   r2 <= data_in_b;
+                                 SEL_3:   r3 <= data_in_b;
+                              endcase
+                        end   
+               SEL_2:   begin
+                           r2 <= data_in_a;
+                           if(we_b)
+                              case(sel_write_b)
+                                 SEL_0:   r0 <= data_in_b;
+                                 SEL_1:   r1 <= data_in_b;
+                                 SEL_3:   r3 <= data_in_b;
+                              endcase
+                        end   
+               SEL_3:   begin
+                           r3 <= data_in_a;
+                           if(we_b)
+                              case(sel_write_b)
+                                 SEL_0:   r0 <= data_in_b;
+                                 SEL_1:   r1 <= data_in_b;
+                                 SEL_2:   r2 <= data_in_b;
+                              endcase
+                        end   
+            endcase
       end
    end     
 endmodule
