f47667fb0c022763a492b890d960e7f0c8fb157c
Ashley J. Robinson <ashley181291@gmail.com>
2015-09-05 16:49:40 +0100

    ir and works

--- up_instruction_register/up_instruction_register.v
+++ up_instruction_register/up_instruction_register.v
@@ -1,6 +1,23 @@
 module up_instruction_register(
-	input	clk,
-	input	nRst
+	input	   wire        clk,
+	input 	wire        nRst,
+   input    wire        we,
+   input    wire        sel,
+   input    wire  [7:0] in,
+   output   reg   [3:0] ir
 );
 
+   parameter   TOP      = 1'b1,
+               BOTTOM   = 1'b0;
+
+   always@(posedge clk or negedge nRst) begin
+      if(!nRst)
+         ir <= 0;
+      else begin
+         case({we,sel})
+            {1'b1,TOP}:    ir <= in[7:4];
+            {1'b1,BOTTOM}: ir <= in[3:0];
+         endcase
+      end
+   end
 endmodule
--- up_instruction_register/up_instruction_register_tb.v
+++ up_instruction_register/up_instruction_register_tb.v
@@ -2,12 +2,20 @@ module up_instruction_register_tb;
 
 	parameter CLK_PERIOD = 20;
 
-	reg clk;
-	reg nRst;
+	reg         clk;
+	reg         nRst;
+   reg         we;
+   reg         sel;
+   reg   [7:0] in;
+   wire  [3:0] ir;
 
 	up_instruction_register up_instruction_register(
-		.clk	(clk),
-		.nRst	(nRst)
+		.clk	(clk  ),
+		.nRst	(nRst ),
+      .we   (we   ),
+      .sel  (sel  ),
+      .in   (in   ),
+      .ir   (ir   )
 	);
 
 	initial begin
@@ -22,9 +30,21 @@ module up_instruction_register_tb;
 	end
 
 	initial begin
-					nRst = 1;
-		#100		nRst = 0;
-		#100		nRst = 1;
+					nRst  = 1;
+               we    = 0;
+               sel   = 0;
+               in    = 0;
+
+		#100		nRst  = 0;
+		#100		nRst  = 1;
+
+      #100     in    = 8'hAB;
+      #100     we    = 1;
+      #100     we    = 0;
+      #100     sel   = 1;
+      #100     we    = 1;
+      #100     we    = 0; 
+
 		#10000
 		$finish;
 	end
