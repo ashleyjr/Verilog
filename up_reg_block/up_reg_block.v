3a23d980af519b2d097fe54d2240b1165bd4b8ad
Ashley J. Robinson <ashley181291@gmail.com>
2015-09-05 13:45:30 +0100

    alu siming and regs in right place

--- up_reg_block/up_reg_block.v
+++ up_reg_block/up_reg_block.v
@@ -1,6 +1,91 @@
 module up_reg_block(
-	input	clk,
-	input	nRst
+	input	   wire        clk,
+	input	   wire        nRst,
+   input    wire [1:0]  sel_out_a,
+   input    wire [1:0]  sel_out_b,
+   input    wire [1:0]  sel_write_a,
+   input    wire [1:0]  sel_write_b,
+   input    wire        we_a,
+   input    wire        we_b,
+   input    wire [7:0]  data_in_a,
+   input    wire [7:0]  data_in_b,
+   output   wire [7:0]  data_out_a,
+   output   wire [7:0]  data_out_b
+
 );
 
+   parameter   REG_ON_RES_0   = 8'h01,
+               REG_ON_RES_1   = 8'h02,
+               REG_ON_RES_2   = 8'h03,
+               REG_ON_RES_3   = 8'h04;
+
+   parameter   SEL_0          = 2'b00,
+               SEL_1          = 2'b01,
+               SEL_2          = 2'b10,
+               SEL_3          = 2'b11;
+
+   reg [7:0]   r0;
+   reg [7:0]   r1;
+   reg [7:0]   r2;
+   reg [7:0]   r3;   
+
+   assign   data_out_a = 
+               (sel_out_a == SEL_0) ? r0:
+               (sel_out_a == SEL_1) ? r1:
+               (sel_out_a == SEL_2) ? r2: r3;
+
+   assign   data_out_b = 
+               (sel_out_b == SEL_0) ? r0:
+               (sel_out_b == SEL_1) ? r1:
+               (sel_out_b == SEL_2) ? r2: r3;
+
+ 
+   always@(posedge clk or negedge nRst) begin 
+      if(!nRst) begin
+         r0 <= REG_ON_RES_0;
+         r1 <= REG_ON_RES_1;
+         r2 <= REG_ON_RES_2;
+         r3 <= REG_ON_RES_3;
+      end else begin
+         if(we_a) 
+            case(sel_write_a) 
+               SEL_0:   begin
+                           r0 <= data_in_a;
+                           if(we_b)
+                              case(sel_write_b)
+                                 SEL_1:   r1 <= data_in_b;
+                                 SEL_2:   r2 <= data_in_b;
+                                 SEL_3:   r3 <= data_in_b;
+                              endcase
+                        end    
+               SEL_1:   begin
+                           r1 <= data_in_a;
+                           if(we_b)
+                              case(sel_write_b)
+                                 SEL_0:   r0 <= data_in_b;
+                                 SEL_2:   r2 <= data_in_b;
+                                 SEL_3:   r3 <= data_in_b;
+                              endcase
+                        end   
+               SEL_2:   begin
+                           r2 <= data_in_a;
+                           if(we_b)
+                              case(sel_write_b)
+                                 SEL_0:   r0 <= data_in_b;
+                                 SEL_1:   r1 <= data_in_b;
+                                 SEL_3:   r3 <= data_in_b;
+                              endcase
+                        end   
+               SEL_3:   begin
+                           r3 <= data_in_a;
+                           if(we_b)
+                              case(sel_write_b)
+                                 SEL_0:   r0 <= data_in_b;
+                                 SEL_1:   r1 <= data_in_b;
+                                 SEL_2:   r2 <= data_in_b;
+                              endcase
+                        end   
+            endcase
+      end
+   end     
 endmodule
--- up_reg_block/up_reg_block_tb.v
+++ up_reg_block/up_reg_block_tb.v
@@ -2,13 +2,34 @@ module up_reg_block_tb;
 
 	parameter CLK_PERIOD = 20;
 
-	reg clk;
-	reg nRst;
+	reg            clk;
+	reg            nRst;
+   reg   [1:0]    sel_out_a;
+   reg   [1:0]    sel_out_b;
+   reg   [1:0]    sel_write_a;
+   reg   [1:0]    sel_write_b;
+   reg            we_a;
+   reg            we_b;
+   reg   [7:0]    data_in_a;
+   reg   [7:0]    data_in_b;
+   wire  [7:0]    data_out_a;
+   wire  [7:0]    data_out_b;
+
 
 	up_reg_block up_reg_block(
-		.clk	(clk),
-		.nRst	(nRst)
-	);
+		.clk	         (clk           ),
+		.nRst	         (nRst          ),
+	   .sel_out_a     (sel_out_a     ),
+      .sel_out_b     (sel_out_b     ),
+      .sel_write_a   (sel_write_a   ),
+      .sel_write_b   (sel_write_b   ),
+      .we_a          (we_a          ),
+      .we_b          (we_b          ),
+      .data_in_a     (data_in_a     ),
+      .data_in_b     (data_in_b     ),
+      .data_out_a    (data_out_a    ),
+      .data_out_b    (data_out_b    ) 
+   );
 
 	initial begin
 		while(1) begin
@@ -22,10 +43,126 @@ module up_reg_block_tb;
 	end
 
 	initial begin
-					nRst = 1;
-		#100		nRst = 0;
-		#100		nRst = 1;
-		#10000
+					nRst           = 1;
+               sel_out_a      = 0;
+               sel_out_b      = 0;
+               sel_write_a    = 0;
+               sel_write_b    = 0;
+               we_a           = 0;
+               we_b           = 0;
+               data_in_a      = 0;
+               data_in_b      = 0;
+
+
+		#100		nRst           = 0;
+		#100		nRst           = 1;
+
+      // scan the outputs
+      #50      sel_out_a   = 2'b00;
+      #50      sel_out_a   = 2'b01;
+      #50      sel_out_a   = 2'b10;
+      #50      sel_out_a   = 2'b11;
+
+	
+      #50      sel_out_b   = 2'b00;
+      #50      sel_out_b   = 2'b01;
+      #50      sel_out_b   = 2'b10;
+      #50      sel_out_b   = 2'b11;
+
+
+      // Write individual regs
+      #50      data_in_a      = 8'hAA;
+               data_in_b      = 8'hBB;        
+               sel_write_a    = 2'b00;
+               sel_write_b    = 2'b01;
+      #50      we_a           = 1;
+               we_b           = 1;
+      #20      we_a           = 0;
+               we_b           = 0;
+
+      #50      data_in_a      = 8'hCC;
+               data_in_b      = 8'hDD;        
+               sel_write_a    = 2'b10;
+               sel_write_b    = 2'b11;
+      #50      we_a           = 1; 
+               we_b           = 1;
+      #20      we_a           = 0;
+               we_b           = 0;
+
+
+               
+      // scan the outputs
+      #50      sel_out_a   = 2'b00;
+      #50      sel_out_a   = 2'b01;
+      #50      sel_out_a   = 2'b10;
+      #50      sel_out_a   = 2'b11;
+
+	
+      #50      sel_out_b   = 2'b00;
+      #50      sel_out_b   = 2'b01;
+      #50      sel_out_b   = 2'b10;
+      #50      sel_out_b   = 2'b11;
+
+      
+      
+      
+      // Write individual regs
+      
+      #50      data_in_a      = 8'h00;
+               data_in_b      = 8'h11;
+               sel_write_a    = 2'b00;
+               sel_write_b    = 2'b00;
+      #50      we_a           = 1; 
+               we_b           = 1;
+      #20      we_a           = 0;
+               we_b           = 0;
+
+
+
+      #50      data_in_a      = 8'h22;
+               data_in_b      = 8'h33;
+               sel_write_a    = 2'b01;
+               sel_write_b    = 2'b01;
+      #50      we_a           = 1; 
+               we_b           = 1;
+      #20      we_a           = 0;
+               we_b           = 0;
+
+
+
+
+
+
+      #50      data_in_a      = 8'h44;
+               data_in_b      = 8'h55;
+               sel_write_a    = 2'b10;
+               sel_write_b    = 2'b10;
+      #50      we_a           = 1; 
+               we_b           = 1;
+      #20      we_a           = 0;
+               we_b           = 0;
+
+
+
+
+
+      #50      data_in_a      = 8'h66;
+               data_in_b      = 8'h77;
+               sel_write_a    = 2'b11;
+               sel_write_b    = 2'b11;
+      #50      we_a           = 1; 
+               we_b           = 1;
+      #20      we_a           = 0;
+               we_b           = 0;
+
+
+
+
+
+      #100     nRst        = 0;
+      #100     nRst        = 1;
+      
+      #10000
 		$finish;
 	end
 
