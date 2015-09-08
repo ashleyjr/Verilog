fe466c64b25c2dc4cda25ef3f1d57d319316b940
Ashley Robinson <ashley181291@gmail.com>
2015-09-08 16:03:16 +0100

    lots less code!

--- up_datapath/up_datapath.v
+++ up_datapath/up_datapath.v
@@ -1,56 +1,17 @@
 module up_datapath(
-   // ip_datapath
 	input	   wire        clk,
 	input	   wire        nRst,
    input    wire  [7:0] data_in,
-   // up_alu
-   input    wire        a_sel_in_a,
-   input    wire        a_sel_in_b,
-   input    wire  [3:0] a_op,
-   // up_instruction_register
+   input    wire  [4:0] op,
    input    wire        ir_we,
-   // up_program_counter
    input    wire        pc_we,
-   // up_reg_block
-   input    wire  [1:0] rb_sel_out_a,
-   input    wire  [1:0] rb_sel_out_b,
    input    wire  [2:0] rb_sel_in,
    input    wire        rb_we,
-   // up_stack_pointer
    input    wire        sp_we,
-   // outputs
    output   wire  [7:0] data_out,
    output   reg   [3:0] ir
    
 );
-      
-   // Parameters
-   parameter   REG_ON_RES_0   = 8'h01,
-               REG_ON_RES_1   = 8'h02,
-               REG_ON_RES_2   = 8'h03,
-               REG_ON_RES_3   = 8'h04;
-
-   parameter   SEL_I0         = 3'b000,
-               SEL_I1         = 3'b001,
-               SEL_I2         = 3'b010,
-               SEL_I3         = 3'b011,
-               SEL_O0         = 3'b100,
-               SEL_O1         = 3'b101,
-               SEL_O2         = 3'b110,
-               SEL_O3         = 3'b111;
-
-   parameter   ADD            = 4'h0,
-               SUB            = 4'h1,
-               MUL            = 4'h2,
-               DIV            = 4'h3,
-               NAND           = 4'h4,
-               NOR            = 4'h5,
-               XOR            = 4'h6,
-               B              = 4'h7;
-
-   parameter   TOP            = 1'b1,
-               BOTTOM         = 1'b0;
-
 
    reg   [7:0]    sp;
    reg   [7:0]    pc;
@@ -59,64 +20,54 @@ module up_datapath(
    reg   [7:0]    r2;
    reg   [7:0]    r3;  
 
-
-   wire  [7:0]    a_data_in_a;
-   wire  [7:0]    a_data_in_b;
-   
-   wire  [7:0]    rb_data_out_a;
-   wire  [7:0]    rb_data_out_b;
-
-   assign   rb_data_out_a = 
-               (rb_sel_out_a == SEL_0)    ? r0:
-               (rb_sel_out_a == SEL_1)    ? r1:
-               (rb_sel_out_a == SEL_2)    ? r2: r3;
-
-   assign   rb_data_out_b = 
-               (rb_sel_out_b == SEL_0)    ? r0:
-               (rb_sel_out_b == SEL_1)    ? r1:
-               (rb_sel_out_b == SEL_2)    ? r2: r3;
-
-   assign   a_data_in_a = 
-               (a_sel_in_a)               ? rb_data_out_a : sp;
-
-   assign   a_data_in_b = 
-               (a_sel_in_b)               ? rb_data_out_b : pc;      
-
    assign   data_out =
-               (op == ADD  )              ? a_data_in_a +  a_data_in_b  :
-               (op == SUB  )              ? a_data_in_a -  a_data_in_b  :
-               (op == MUL  )              ? a_data_in_a *  a_data_in_b  :
-               (op == DIV  )              ? a_data_in_a /  a_data_in_b  :
-               (op == NAND )              ? a_data_in_a ~& a_data_in_b  :
-               (op == NOR  )              ? a_data_in_a ~| a_data_in_b  :
-               (op == XOR  )              ? a_data_in_a ^  a_data_in_b  : 
-               (op == B    )              ? a_data_in_b                 : 8'h00;
+               (op == 5'b00000   )        ? r1 +  r2           :
+               (op == 5'b00001   )        ? r1 -  r2           :
+               (op == 5'b00010   )        ? r1 *  r2           :
+               (op == 5'b00011   )        ? r1 /  r2           :
+               (op == 5'b00100   )        ? r1 ~& r2           :
+               (op == 5'b00101   )        ? r1 ~| r2           :
+               (op == 5'b00110   )        ? r0                 :  
+               (op == 5'b00111   )        ? r1                 :
+               (op == 5'b01000   )        ? r2                 :
+               (op == 5'b01001   )        ? r3                 : 
+               (op == 5'b01010   )        ? r0 ^ r1            :
+               (op == 5'b01011   )        ? r1 ^ r2            :
+               (op == 5'b01100   )        ? r2 ^ r3            :
+               (op == 5'b01101   )        ? sp ^ r3            :
+               (op == 5'b01110   )        ? pc ^ r3            :
+               (op == 5'b01111   )        ? sp + 8'h01         :
+               (op == 5'b10000   )        ? sp - 8'h01         :
+               (op == 5'b10001   )        ? pc                 :
+               (op == 5'b10010   )        ? (pc >> 1 & 8'h7F)  : 
+               (op == 5'b10011   )        ? (pc >> 1 | 8'h80)  :
+                                            data_in            ;  
 
    always@(posedge clk or negedge nRst) begin
       if(!nRst) begin
          pc <= 8'h00;
          sp <= 8'hFF;
          ir <= 4'h0;
-         r0 <= REG_ON_RES_0;
-         r1 <= REG_ON_RES_1;
-         r2 <= REG_ON_RES_2;
-         r3 <= REG_ON_RES_3;  
+         r0 <= 8'h01;
+         r1 <= 8'h02;
+         r2 <= 8'h03;
+         r3 <= 8'h04;
       end else begin
          if(ir_we)
-            if(pc[0])   ir <= data_in[7:4];
-            else        ir <= data_in[3:0];
+            if(pc[0])   ir <= data_out[7:4];
+            else        ir <= data_out[3:0];
          if(sp_we)      sp <= data_out;
          if(pc_we)      pc <= data_out;
          if(rb_we) 
             case(rb_sel_in) 
-               SEL_I0:  r0 <= data_in;  
-               SEL_I1:  r1 <= data_in;
-               SEL_I2:  r2 <= data_in;
-               SEL_I3:  r3 <= data_in;
-               SEL_O0:  r0 <= data_out;  
-               SEL_O1:  r1 <= data_out;
-               SEL_O2:  r2 <= data_out;
-               SEL_O3:  r3 <= data_out;
+               3'b000:  r0 <= data_in;       // SEL_I0
+               3'b001:  r1 <= data_in;       // SEL_I1
+               3'b010:  r2 <= data_in;       // SEL_I2
+               3'b011:  r3 <= data_in;       // SEL_I3
+               3'b100:  r0 <= data_out;      // SEL_00
+               3'b101:  r1 <= data_out;      // SEL_01
+               3'b110:  r2 <= data_out;      // SEL_02
+               3'b111:  r3 <= data_out;      // SEL_03
             endcase
       end
    end
