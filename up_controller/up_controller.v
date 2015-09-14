3ef894b404b9a67ec11adf427bd392d2f8ef4510
Ashley Robinson <ashley181291@gmail.com>
2015-09-14 14:01:49 +0100

    new combo structure

--- up_controller/up_controller.v
+++ up_controller/up_controller.v
@@ -1,17 +1,17 @@
 module up_controller(
-	input			   clk,
-	input			   nRst,
-	input			   int,
-	input	   [3:0]	ir,
-	input 			mem_re,
-	output	[4:0]	op,
-	output			ir_we,
-	output			pc_we,
-	output	[2:0]	rb_sel_in,
-	output 			rb_we,
-	output 			sp_we,
-	output 			mem_we,
-	output			ale
+	input			         clk,
+	input			         nRst,
+	input			         int,
+	input	     [3:0]	   ir,
+	input 			      mem_re,
+	output reg [4:0]     op,
+	output reg			   ir_we,
+	output reg           pc_we,
+	output reg [2:0]     rb_sel_in,
+	output reg           rb_we,
+	output reg           sp_we,
+	output reg           mem_we,
+	output reg           ale
 );
 
    parameter   FETCH_LATCH    = 3'b001,
@@ -22,54 +22,44 @@ module up_controller(
 
    reg [2:0]   state;
 
-   assign op = 
-            (state == FETCH_LATCH)                          ?  5'b01101 :  // PC shifted right out
-            (state == FETCH_READ )                          ?  5'b01111 :  // PC + 1 out
-                                                               {1'b0,ir};  // op is then defined the ir
-   
 
-   assign ir_we = 
-            (state == FETCH_READ)                           ?  1'b1 : 1'b0;
+   always @*
+      case(state)
+         {FETCH_LATCH}:    begin
+                              op          = 5'b11101;  // PC shifted right out
+                              ir_we       = 1'b0;
+                              pc_we       = 1'b0;
+                              rb_sel_in   = 3'b000;
+                              rb_we       = 1'b0;
+                              sp_we       = 1'b0;
+                              mem_we      = 1'b0;
+                              ale         = 1'b1;
+                           end
+         {FETCH_READ}:     begin
+                              op          = 5'b11111;  // PC + 1 out
+                              ir_we       = 1'b0;
+                              pc_we       = 1'b0;
+                              rb_sel_in   = 3'b000;
+                              rb_we       = 1'b0;
+                              sp_we       = 1'b0;
+                              mem_we      = 1'b0;
+                              ale         = 1'b1;
+                           end
+         {EXECUTE_1}:      always @*
+                              case(ir)
+                                 4'b0000: begin
+                                             op          = 5'b11111;  // PC + 1 out
+                                             ir_we         = 1'b0;
+                                             pc_we       = 1'b0;
+                                             rb_sel_in   = 3'b000;
+                                             rb_we       = 1'b0;
+                                             sp_we       = 1'b0;
+                                             mem_we      = 1'b0;
+                                             ale         = 1'b1;
+                                          end
+                              endcase
 
-   assign pc_we = 
-            (state == FETCH_READ)                           ?  1'b1 : 1'b0;
-
-
-   assign rb_sel_in = 
-            ((state == EXECUTE_1) && (ir == 4'b00??))       ?  3'b100   :  // Group add, sub, mul and div
-
-            ((state == EXECUTE_1) && (ir == 4'b0101))       ?  3'b100   :
-            ((state == EXECUTE_2) && (ir == 4'b0101))       ?  3'b101   :
-            ((state == EXECUTE_3) && (ir == 4'b0101))       ?  3'b100   :
-            ((state == EXECUTE_1) && (ir == 4'b0110))       ?  3'b101   :
-            ((state == EXECUTE_2) && (ir == 4'b0110))       ?  3'b110   :
-            ((state == EXECUTE_3) && (ir == 4'b0110))       ?  3'b101   :
-            ((state == EXECUTE_1) && (ir == 4'b0111))       ?  3'b110   :
-            ((state == EXECUTE_2) && (ir == 4'b0111))       ?  3'b111   :
-            ((state == EXECUTE_3) && (ir == 4'b0111))       ?  3'b110   :
-                                                               3'b000   ;
-
-   assign rb_we = 
-            ((state == EXECUTE_1) && (ir == 4'b0000))      ?  1'b1  : 
-            ((state == EXECUTE_1) && (ir == 4'b0001))      ?  1'b1  : 
-            ((state == EXECUTE_1) && (ir == 4'b0010))      ?  1'b1  : 
-            ((state == EXECUTE_1) && (ir == 4'b0011))      ?  1'b1  : 
-            ((state == EXECUTE_1) && (ir == 4'b0100))      ?  1'b1  :
-            ((state == EXECUTE_1) && (ir == 4'b0101))      ?  1'b1  :
-            ((state == EXECUTE_2) && (ir == 4'b0101))      ?  1'b1  :
-            ((state == EXECUTE_3) && (ir == 4'b0101))      ?  1'b1  :
-            ((state == EXECUTE_1) && (ir == 4'b0110))      ?  1'b1  :
-            ((state == EXECUTE_2) && (ir == 4'b0110))      ?  1'b1  :
-            ((state == EXECUTE_3) && (ir == 4'b0110))      ?  1'b1  :
-            ((state == EXECUTE_1) && (ir == 4'b0111))      ?  1'b1  :
-            ((state == EXECUTE_2) && (ir == 4'b0111))      ?  1'b1  :
-            ((state == EXECUTE_3) && (ir == 4'b0111))      ?  1'b1  :
-
-                                                               1'b0;
-
-   assign ale = 
-            (state == FETCH_LATCH)                       ? 1'b1  : 1'b0;
-
+      endcase
 
 
    always@(posedge clk or negedge nRst) begin
