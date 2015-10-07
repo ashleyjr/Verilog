7054e34a5cc8276761d68fcbea552205319272ee
Ashley Robinson <ashley181291@gmail.com>
2015-10-07 17:46:23 +0100

    3 functions in code

--- up_memory/code.asm
+++ up_memory/code.asm
@@ -1,7 +1,7 @@
 # -------------Init table 
-01          #  R0 - Jump back
-3F          #  R1 - First function
-42          #  R2 - Second function
+75          #  R0 - Third function
+5C          #  R1 - First function
+69          #  R2 - Second function
 7F          #  R3 - Address if variable
 
       # Setup
@@ -55,6 +55,44 @@
 
 
 
+
+
+      # Jump to address in R3
+      # All other regs to be saved
+      PUSH
+      SW12
+      PUSH
+      SW01
+      SW12
+      PUSH
+
+      REF
+      SW01
+      SW12
+      REF
+      SW01
+      SW23
+      PUSHC
+      SW23
+      BE
+      SW23
+      POP
+
+      POP
+      SW12
+      SW01
+      POP
+      SW12
+      POP
+
+
+      SW01
+      SW12
+      SW23
+
+
+
+
       # Jump to address in R3
       # All other regs to be saved
       PUSH
@@ -86,6 +124,11 @@
 
       SW23
       SW12
+      SW01
+      SW23
+      SW12
+
+
 
 
       POPC
@@ -94,6 +137,18 @@
 
 # Function 1 - 
 
+      SW23
+      PUSH
+
+
+
+      POP
+      SW23
+      REF
+      SW01
+      SW12
+      REF
+      SW01
       ADD
       SW01
       POPC
@@ -102,7 +157,40 @@
 
 
 # Function 2 - 
+      
 
+      SW23
+      PUSH
+
+
+
+      POP
+      SW23
+      REF
+      SW01
+      SW12
+      REF
+      SW01
+      ADD
+      SW01
+      POPC
+
+
+# Function 3 - 
+      
+
+      SW23
+      PUSH
+
+
+
+      POP
+      SW23
+      REF
+      SW01
+      SW12
+      REF
+      SW01
       ADD
       SW01
       POPC
