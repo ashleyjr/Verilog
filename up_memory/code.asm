18086ff898b594f98d4ac286a2278824d65757e8
Ashley Robinson <ashley181291@gmail.com>
2015-10-07 16:14:57 +0100

    function test stuff working but needs to be tidied up

--- up_memory/code.asm
+++ up_memory/code.asm
@@ -1,91 +1,108 @@
 # -------------Init table 
 01          #  R0 - Jump back
-1E          #  R1 - First function
-00          #  R2 - Blank
+3F          #  R1 - First function
+42          #  R2 - Second function
 7F          #  R3 - Address if variable
 
       # Setup
       STW         # R2 @ 0x7F
             
+
+      SW12
+      SW23
+
+
+
       # Loop
       PUSHC
 
-      PUSHC
-      POP
-      SW01
-      ADD
+
+
+
+
+
+      # Jump to address in R3
+      # All other regs to be saved
+      PUSH
+      SW12
+      PUSH
       SW01
       SW12
       PUSH
-      POPC
+
+      REF
+      SW01
+      SW12
+      REF
+      SW01
+      SW23
+      PUSHC
+      SW23
+      BE
+      SW23
+      POP
+
+      POP
+      SW12
+      SW01
+      POP
+      SW12
+      POP
+
+
+      SW12
+      SW23
+
+
+
+      # Jump to address in R3
+      # All other regs to be saved
+      PUSH
+      SW12
+      PUSH
+      SW01
+      SW12
+      PUSH
+
+      REF
+      SW01
+      SW12
+      REF
+      SW01
+      SW23
+      PUSHC
+      SW23
+      BE
+      SW23
+      POP
+
+      POP
+      SW12
+      SW01
+      POP
+      SW12
+      POP
+
+
+      SW23
       SW12
 
 
-
-
-
       POPC
+
 
 
 # Function 1 - 
 
-      PUSH
-      SW23
-      PUSH
-      SW12
-      PUSH
+      ADD
       SW01
-      SW12
-      PUSH        # Subroutine code
-
-      POP
-      SW12
-      SW01
-      POP
-      SW12
-      POP
-      SW23
-      POP
       POPC
+
+
+
 
 # Function 2 - 
 
-      PUSH
-      SW23
-      PUSH
-      SW12
-      PUSH
+      ADD
       SW01
-      SW12
-      PUSH        # Subroutine code
-
-      POP
-      SW12
-      SW01
-      POP
-      SW12
-      POP
-      SW23
-      POP
-      POPC
-
-# Function 3 - 
-
-      PUSH
-      SW23
-      PUSH
-      SW12
-      PUSH
-      SW01
-      SW12
-      PUSH        # Subroutine code
-
-      POP
-      SW12
-      SW01
-      POP
-      SW12
-      POP
-      SW23
-      POP
       POPC
