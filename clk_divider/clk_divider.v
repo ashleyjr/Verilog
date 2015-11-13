61bd98c64dab481adb61ca6c5e78e0ff7ee5099e
Ashley Robinson <ashley181291@gmail.com>
2015-11-13 10:20:46 +0000

    clk divider working

--- clk_divider/clk_divider.v
+++ clk_divider/clk_divider.v
@@ -3,34 +3,38 @@ module clk_divider(
 	input             nRst,
    input             update,
    input    [31:0]   div,
-   output   reg      div_clk
-
+   output            div_clk
 );
 
    reg [31:0]  top;
    reg [31:0]  count;
+   reg         div_reg;
 
    wire        hit;
 
-   assign hit = (count == div) ? 1'b1 : 1'b0;
+   assign hit     = (count == ((top >> 2)) )    ? 1'b1 : 1'b0;
+   
+   assign div_clk = (   (top     == 32'd0) &&
+                        (update  == 1'b0)    )  ? clk  : div_reg;
 
    always@(posedge clk or negedge nRst) begin 
       casex({nRst,update,hit})
          3'b0xx:  begin
-                     top      <= 32'd0;
+                     top      <= 32'd1;
                      count    <= 32'd0;
-                     div_clk  <= 1'b0;        
+                     div_reg  <= 1'b0;        
                   end
          3'b11x:  begin
                      top      <= div;
                      count    <= 32'd0;
+                     div_reg  <= 1'b0;
                   end
          3'b100:  begin
                      count    <= count + 1'b1;
                   end
          3'b101:  begin
                      count    <= 32'd0;
-                     div_clk  <= ~div_clk;
+                     div_reg  <= ~div_reg;
                   end
       endcase
    end
--- clk_divider/clk_divider_tb.v
+++ clk_divider/clk_divider_tb.v
@@ -37,23 +37,61 @@ module clk_divider_tb;
 		
 
 
-      // Set up to divide input clock by 10
+      // Set up to divide input clock by 2
       #1000    update   = 1;
-      #100     div      = 32'd10;
+      #100     div      = 32'd2;
       #100     update   = 0;
       #1000
+
+      // Set up to divide input clock by 3
+      #1000    update   = 1;
+      #100     div      = 32'd3;
+      #100     update   = 0;
+      #10000
+
+      // Set up to divide input clock by 4
+      #1000    update   = 1;
+      #100     div      = 32'd4;
+      #100     update   = 0;
+      #1000
+
+	// Set up to divide input clock by 5
+      #1000    update   = 1;
+      #100     div      = 32'd5;
+      #100     update   = 0;
+      #1000
+
+      // Set up to divide input clock by 400
+      #1000    update   = 1;
+      #100     div      = 32'd400;
+      #100     update   = 0;
+      #10000
 
       // Set up to divide input clock by 100
       #1000    update   = 1;
       #100     div      = 32'd100;
       #100     update   = 0;
-      #10000
+      #1000
 
-      // Set up to divide input clock by 0
+      // Set up to divide input clock by 50
       #1000    update   = 1;
-      #100     div      = 32'd0;
+      #100     div      = 32'd50;
       #100     update   = 0;
       #1000
+
+      // Set up to divide input clock by 20
+      #1000    update   = 1;
+      #100     div      = 32'd20;
+      #100     update   = 0;
+      #10000
+
+
+      // Change div input
+      #1000 		div = 32'd10;
+      #1000 		div = 32'd345;
+      #1000 		div = 32'd1345;
+      #1000 		div = 32'd3453;
+      #1000 		div = 32'd3;
 
 		$finish;
 	end
