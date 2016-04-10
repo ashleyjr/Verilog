1c8df3d7601bab1e7abb7d695a4b3dd5200c27a5
Ashley J. Robinson <ashley181291@gmail.com>
2016-04-10 17:46:00 +0100

    load code writing down the memory

--- up/code/load_code.asm
+++ up/code/load_code.asm
@@ -1,12 +1,25 @@
 # -------------Init table 
 01          #  R0 
 FE          #  R1 
-00          #  R2
-7E          #  R3
+FF          #  R2
+7F          #  R3
 
-
+      
+      # Loop
+      PUSHC
+               # Load addr in R3, store addr in R1
       LDW      # Read mem map data in to R2           
-      SW12
-      SW23
-      SW12
-      STW      # Store data from mem map at 0x7E 
+      SW12     # Load addr in R3, store addr in R2, data in R1
+      SW23     # Load addr in R2, store addr in R3, data in R1
+      SW12     # Load addr in R1, store addr in R3, data in R2
+      STW      # Store data
+      SW23     # Load addr in R1, store addr in R2, data in R3
+      SW12     # Load addr in R2, store addr in R1, data in R3
+      SW23     # Load addr in R3, store addr in R1, data in R2
+      SW01     # Load addr in R3, store addr in R0, data in R2, 1 in R1
+      SW12     # Load addr in R3, store addr in R0, data in R1, 1 in R2
+      SW01     # Load addr in R3, store addr in R1, data in R0, 1 in R2
+      SUB      # Load addr in R3, store addr in R1, store addr - 1 in R0, 1 in R2
+      SW12     # Load addr in R3, store addr in R2, store addr - 1 in R0, 1 in R1
+      SW01     # Load addr in R3, store addr in R2, store addr - 1 in R1, 1 in R0
+      POPC
