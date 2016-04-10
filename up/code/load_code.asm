871ce1a58c851b7694538f6e9c0a505e0e4cb8ad
Ashley J. Robinson <ashley181291@gmail.com>
2016-04-10 15:49:24 +0100

    placeholder for code the will load proper code

---
+++ up/code/load_code.asm
@@ -0,0 +1,17 @@
+# -------------Init table 
+00          #  R0 
+00          #  R1 
+00          #  R2
+00          #  R3
+
+
+      REF
+      SW01
+      SW12
+      SW23
+      REF
+      SW01
+      SW12
+      REF
+      SW01
+      REF 
