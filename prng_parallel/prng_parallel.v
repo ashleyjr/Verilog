cd4453fe52b4b364eab9119b14470d5c983973f2
Ashley J. Robinson <ashley181291@gmail.com>
2016-05-14 21:35:42 +0100

    9 bit prng in 8 bit parallel

--- prng_parallel/prng_parallel.v
+++ prng_parallel/prng_parallel.v
@@ -6,14 +6,33 @@ module prng_parallel(
    input    [7:0] seed,
    output   [7:0] rand
 );
+   
+   wire [8:0]  rands [7:0];
 
-   prng #(8,1,2)  prng(
-      .clk     (clk     ),
-      .nRst    (nRst    ),
-      .update  (update  ),
-      .reseed  (reseed  ),
-      .seed    (seed    ),
-      .rand    (rand    )
-   );
+   assign rand =  {
+                     rands[7][8],
+                     rands[6][8],
+                     rands[5][8],
+                     rands[4][8],
+                     rands[3][8],
+                     rands[2][8],
+                     rands[1][8],
+                     rands[0][8]
+                  };
+   genvar i;
+   generate
+      for(i=0;i<8;i=i+1) begin:prng
+         prng #(9,(i+1),0) prng(
+            .clk     (clk        ),
+            .nRst    (nRst       ),
+            .update  (update     ),
+            .reseed  (reseed     ),
+            .seed    ({seed,1'b0}),
+            .rand    (rands[i]   )
+         );
+      end
+   endgenerate
+
+
 
 endmodule
