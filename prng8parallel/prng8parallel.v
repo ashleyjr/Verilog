8f0a3b022f44474d28c5965d54a4a36d838bbb6a
Ashley J. Robinson <ashley181291@gmail.com>
2016-05-14 19:20:30 +0100

    gen prngs

--- prng8parallel/prng8parallel.v
+++ prng8parallel/prng8parallel.v
@@ -9,12 +9,21 @@ module prng8parallel(
    
    parameter WIDTH = 8;
 
-   prng8 prng8(
-      .clk     (clk     ),
-      .nRst    (nRst    ),
-      .update  (update  ),
-      .reseed  (reseed  ),
-      .seed    (seed    ),
-      .rand    (rand    )
-   );
+   wire  [WIDTH-1:0] pass  [WIDTH:0];
+
+   assign pass[0] = seed;
+
+   genvar i;
+   generate
+      for(i=0;i<WIDTH;i=i+1) begin:prng8
+         prng8 prng8(
+            .clk     (clk        ),
+            .nRst    (nRst       ),
+            .update  (update     ),
+            .reseed  (reseed     ),
+            .seed    (pass[i]    ),
+            .rand    (pass[i+1]  )
+         );
+      end
+   endgenerate
 endmodule
