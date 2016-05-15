6ba54b8c2cf6dc3faf249d6abb6bd034a945dd5e
Ashley J. Robinson <ashley181291@gmail.com>
2016-05-16 00:21:51 +0100

    removed old prngs

--- prng32/prng32_tb.tcl
+++
@@ -1,8 +0,0 @@
-set nsigs [ gtkwave::getNumFacs ]
-set sigs [list]
-for {set i 0} {$i < $nsigs} {incr i} {
-    set name  [ gtkwave::getFacName $i ]
-    lappend sigs $name
-}
-set added [ gtkwave::addSignalsFromList $sigs ]
-gtkwave::/Time/Zoom/Zoom_Full
