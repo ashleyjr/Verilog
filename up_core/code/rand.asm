2d240f715a5ad718813c7bee394706c15a8be79d
Ashley J. Robinson <ashley181291@gmail.com>
2016-05-18 22:13:55 +0100

    rand may not be possible with no right shift

--- up_core/code/rand.asm
+++ up_core/code/rand.asm
@@ -1,7 +1,7 @@
 # -------------Init table 
 02          #  R0 - Multiply by 2 to shift left
-00          #  R1
-0F          #  R2 - Seed
+02          #  R1
+01          #  R2 - Seed
 F0          #  R3 - Stash address
 
       # Setup
@@ -11,40 +11,68 @@ F0          #  R3 - Stash address
  
       # Loop
       PUSHC    # Start of oop
- 
-         # Seed stored in 0xF0
+          
+         LDW      # R0 = 0x02             R1 = 0x02               R2 = Seed               R3 = Seed address
+         MUL      # R0 = Seed << 1        R1 = 0x02               R2 = Seed               R3 = Seed address
+         PUSH     # R0 = Seed << 1        R1 = 0x02               R2 = X                  R3 = Seed address
+         SW01     # R0 = 0x02             R1 = Seed << 1          R2 = X                  R3 = Seed address
+         SW12     # R0 = 0x02             R1 = X                  R2 = Seed << 1          R3 = Seed address
+         SW01     # R0 = X                R1 = 0x02               R2 = Seed << 1          R3 = Seed address
+         MUL      # R0 = Seed << 2        R1 = 0x02               R2 = Seed << 1          R3 = Seed address
+         SW01     # R0 = 0x02             R1 = Seed << 2          R2 = Seed << 1          R3 = Seed address
+         SW12     # R0 = 0x02             R1 = Seed << 1          R2 = Seed << 2          R3 = Seed address
+         PUSH     # R0 = 0x02             R1 = Seed << 1          R2 = X                  R3 = Seed address
 
-         LDW      # R0 = 0x02          R1 = X               R2 = Seed            R3 = Seed address
-         PUSH     # R0 = 0x02          R1 = X               R2 = X               R3 = Seed address
-         SW01     # R0 = X             R1 = 0x02            R2 = X               R3 = Seed address
-         SW12     # R0 = X             R1 = X               R2 = 0x02            R3 = Seed address
-         REF      # R0 = 0x02          R1 = X               R2 = 0x02            R3 = Seed address
-         SW01     # R0 = X             R1 = 0x02            R2 = 0x02            R3 = Seed address
-         SUB      # R0 = 0x00          R1 = 0x02            R2 = 0x02            R3 = Seed address
-         SW01     # R0 = 0x02          R1 = 0x00            R2 = 0x02            R3 = Seed address
-         NAND     # R0 = 0xFF          R1 = 0x00            R2 = 0x02            R3 = Seed address
-         SW01     # R0 = 0x00          R1 = 0xFF            R2 = 0x02            R3 = Seed address
-         POP      # R0 = 0x00          R1 = 0xFF            R2 = Seed            R3 = Seed address
-         NAND     # R0 = NOT Seed      R1 = 0xFF            R2 = Seed            R3 = Seed address
-         PUSH     # R0 = NOT Seed      R1 = 0xFF            R2 = X               R3 = Seed address
-         SW01     # R0 = 0xFF          R1 = NOT Seed        R2 = X               R3 = Seed address
-         SW12     # R0 = 0xFF          R1 = X               R2 = NOT Seed        R3 = Seed address
-         PUSH     # R0 = 0xFF          R1 = X               R2 = X               R3 = Seed address
-         REF      # R0 = 0x02          R1 = X               R2 = X               R3 = Seed address
-         SW01     # R0 = X             R1 = 0x02            R2 = X               R3 = Seed address
-         SW23     # R0 = X             R1 = 0x02            R2 = Seed address    R3 = X
-         SW12     # R0 = X             R1 = Seed address    R2 = 0x02            R3 = X
-         SUB      # R0 = NOT address   R1 = Seed address    R2 = 0x02            R3 = X
-         SW01     # R0 = Seed address  R1 = NOT address     R2 = 0x02            R3 = X
-         SW12     # R0 = Seed address  R1 = 0x02            R2 = NOT address     R3 = X
-         SW23     # R0 = Seed address  R1 = 0x02            R2 = X               R3 = NOT address
-         POP      # R0 = Seed address  R1 = 0x02            R2 = NOT Seed        R3 = NOT address
-         STW      # R0 = Seed address  R1 = 0x02            R2 = X               R3 = NOT address
- 
+         # Stack = Seed, Seed << 2
 
-         # NOT Seed stored in 0xEE
-         # Seed stored in 0xF0
+         SW01     # R0 = Seed << 1        R1 = 0x02               R2 = X                  R3 = Seed address
+         SW12     # R0 = Seed << 1        R1 = X                  R2 = 0x02               R3 = Seed address
+         REF      # R0 = 0x02             R1 = X                  R2 = 0x02               R3 = Seed address
+         SW01     # R0 = X                R1 = 0x02               R2 = 0x02               R3 = Seed address
+         SUB      # R0 = 0x00             R1 = 0x02               R2 = 0x02               R3 = Seed address
+         SW01     # R0 = 0x02             R1 = 0x00               R2 = 0x02               R3 = Seed address
+         NAND     # R0 = 0xFF             R1 = 0x00               R2 = 0x02               R3 = Seed address
+         SW01     # R0 = 0x00             R1 = 0xFF               R2 = 0x02               R3 = Seed address
+         POP      # R0 = 0x00             R1 = 0xFF               R2 = Seed << 2          R3 = Seed address
+         NAND     # R0 = ~(Seed << 2)     R1 = 0xFF               R2 = Seed << 2          R3 = Seed address
+         PUSH     # R0 = ~(Seed << 2)     R1 = 0xFF               R2 = X                  R3 = Seed address
+         SW01     # R0 = 0xFF             R1 = ~(Seed << 2)       R2 = X                  R3 = Seed address
+         SW12     # R0 = 0xFF             R1 = X                  R2 = ~(Seed << 2)       R3 = Seed address
+         PUSH     # R0 = 0xFF             R1 = X                  R2 = X                  R3 = Seed address
 
+         # Stack = Seed, Seed << 2, ~(Seed << 2)
+
+         
+         PUSH     # R0 = 0x02             R1 = X                  R2 = X                  R3 = Seed address
+         SW01     # R0 = X                R1 = 0x02               R2 = X                  R3 = Seed address
+         SW12     # R0 = X                R1 = X                  R2 = 0x02               R3 = Seed address
+         REF      # R0 = 0x02             R1 = X                  R2 = 0x02               R3 = Seed address
+         SW01     # R0 = X                R1 = 0x02               R2 = 0x02               R3 = Seed address
+         SUB      # R0 = 0x00             R1 = 0x02               R2 = 0x02               R3 = Seed address
+         SW01     # R0 = 0x02             R1 = 0x00               R2 = 0x02               R3 = Seed address
+         NAND     # R0 = 0xFF             R1 = 0x00               R2 = 0x02               R3 = Seed address
+         SW01     # R0 = 0x00             R1 = 0xFF               R2 = 0x02               R3 = Seed address
+         POP      # R0 = 0x00             R1 = 0xFF               R2 = Seed               R3 = Seed address
+         NAND     # R0 = NOT Seed         R1 = 0xFF               R2 = Seed               R3 = Seed address
+         PUSH     # R0 = NOT Seed         R1 = 0xFF               R2 = X                  R3 = Seed address
+         SW01     # R0 = 0x02             R1 = NOT Seed           R2 = X                  R3 = Seed address
+         SW12     # R0 = 0x02             R1 = X                  R2 = NOT Seed           R3 = Seed address
+         SW01     # R0 = X                R1 = 0x02               R2 = NOT Seed           R3 = Seed address
+         MUL      # R0 = (NOT Seed << 2)  R1 = 0x02               R2 = NOT Seed           R3 = Seed address
+
+         
+         SW12     # R0 = NOT Seed      R1 = Seed            R2 = 0xFF            R3 = Seed address
+         PUSH     # R0 = NOT Seed      R1 = Seed            R2 = X               R3 = Seed address
+         SW12     # R0 = NOT Seed      R1 = X               R2 = Seed            R3 = Seed address
+         SW01     # R0 = X             R1 = NOT Seed        R2 = Seed            R3 = Seed address
+         NAND     # R0 = NS NAND S     R1 = NOT Seed        R2 = Seed            R3 = Seed address
+         SW01     # R0 = NOT Seed      R1 = NS NAND S       R2 = Seed            R3 = Seed address
+         POP      # R0 = NOT Seed      R1 = NS NAND S       R2 = 0xFF            R3 = Seed address
+         NAND     # R0 = NS AND S      R1 = NS NAND S       R2 = 0xFF            R3 = Seed address 
+         SW01     # R0 = NS NAND S     R1 = NS AND S        R2 = 0xFF            R3 = Seed address 
+         SW12     # R0 = NS NAND S     R1 = 0xFF            R2 = NS AND S        R3 = Seed address 
+         STW      # R0 = NS NAND S     R1 = 0xFF            R2 = X               R3 = Seed address
+         REF      # R0 = 0x02          R1 = 0xFF            R2 = X               R3 = Seed address
 
 
 
