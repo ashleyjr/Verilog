7dd565fcb9f10978f2e36a3b2d0d7ad9d52192c4
Ashley J. Robinson <ashley181291@gmail.com>
2016-05-18 21:31:21 +0100

    storing a NOT in mem

--- up_core/code/rand.asm
+++ up_core/code/rand.asm
@@ -1,8 +1,8 @@
 # -------------Init table 
 02          #  R0 - Multiply by 2 to shift left
 00          #  R1
-01          #  R2 - Seed
-7F          #  R3 - Stash address
+0F          #  R2 - Seed
+F0          #  R3 - Stash address
 
       # Setup
       STW      # Place seed in stash address
@@ -11,57 +11,43 @@
  
       # Loop
       PUSHC    # Start of oop
-         
-         
-         REF                                                                  # NOT the seed and place on stack
-         SW01
-         SW12
-         REF
-         SW01
-         SUB                  # R0 = 0x00
-            LDW               # Get current seed
-               SW01
-               ADD
-               SW01           # R1 == R2 == Current seed
-                  NAND        # Use a NAND to NOT current seed
-                  SW01
-                  SW12
-                  PUSH        # Put the NOT of current seed on the stack
-         
-        
+ 
+         # Seed stored in 0xF0
 
-         LDW                                                                     # << 2 the seed and place on stack
-         REF                  # Load 0x02 in R0                                        
-            SW01 
-            SW12
-            MUL               # Left shift the NOT
-               SW01
-               SW12
-               MUL            # Left shift the NOT again
-                  SW01
-                  SW12
-                  PUSH        # NOT << 2 on stack
+         LDW      # R0 = 0x02          R1 = X               R2 = Seed            R3 = Seed address
+         PUSH     # R0 = 0x02          R1 = X               R2 = X               R3 = Seed address
+         SW01     # R0 = X             R1 = 0x02            R2 = X               R3 = Seed address
+         SW12     # R0 = X             R1 = X               R2 = 0x02            R3 = Seed address
+         REF      # R0 = 0x02          R1 = X               R2 = 0x02            R3 = Seed address
+         SW01     # R0 = X             R1 = 0x02            R2 = 0x02            R3 = Seed address
+         SUB      # R0 = 0x00          R1 = 0x02            R2 = 0x02            R3 = Seed address
+         SW01     # R0 = 0x02          R1 = 0x00            R2 = 0x02            R3 = Seed address
+         NAND     # R0 = 0xFF          R1 = 0x00            R2 = 0x02            R3 = Seed address
+         SW01     # R0 = 0x00          R1 = 0xFF            R2 = 0x02            R3 = Seed address
+         POP      # R0 = 0x00          R1 = 0xFF            R2 = Seed            R3 = Seed address
+         NAND     # R0 = NOT Seed      R1 = 0xFF            R2 = Seed            R3 = Seed address
+         PUSH     # R0 = NOT Seed      R1 = 0xFF            R2 = X               R3 = Seed address
+         SW01     # R0 = 0xFF          R1 = NOT Seed        R2 = X               R3 = Seed address
+         SW12     # R0 = 0xFF          R1 = X               R2 = NOT Seed        R3 = Seed address
+         PUSH     # R0 = 0xFF          R1 = X               R2 = X               R3 = Seed address
+         REF      # R0 = 0x02          R1 = X               R2 = X               R3 = Seed address
+         SW01     # R0 = X             R1 = 0x02            R2 = X               R3 = Seed address
+         SW23     # R0 = X             R1 = 0x02            R2 = Seed address    R3 = X
+         SW12     # R0 = X             R1 = Seed address    R2 = 0x02            R3 = X
+         SUB      # R0 = NOT address   R1 = Seed address    R2 = 0x02            R3 = X
+         SW01     # R0 = Seed address  R1 = NOT address     R2 = 0x02            R3 = X
+         SW12     # R0 = Seed address  R1 = 0x02            R2 = NOT address     R3 = X
+         SW23     # R0 = Seed address  R1 = 0x02            R2 = X               R3 = NOT address
+         POP      # R0 = Seed address  R1 = 0x02            R2 = NOT Seed        R3 = NOT address
+         STW      # R0 = Seed address  R1 = 0x02            R2 = X               R3 = NOT address
+ 
 
-         
+         # NOT Seed stored in 0xEE
+         # Seed stored in 0xF0
 
-         POP                                                                     # Task two from stack AND then place on stack
-         SW12
-         POP
-            NAND  
-               SW01
-               SW12
-               PUSH           # NAND on stack
-                  REF
-                  SW01
-                  SW12
-                  REF
-                  SW01
-                  SUB         # R0 = 0x00
-                     SW01
-                     POP
-                     NAND        # NOT the value
-                        PUSH     # AND on stack
-                       
+
+
+
       PO   PC     # End of loop
            
            
