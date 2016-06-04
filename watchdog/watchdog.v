02c203660e0e38dc975b261c0ac44babda2bec1b
Ashley J. Robinson <ashley181291@gmail.com>
2016-06-04 20:57:35 +0100

    re gen watchdog and use new timer

--- watchdog/watchdog.v
+++ watchdog/watchdog.v
@@ -1,36 +1,41 @@
 module watchdog(
-	input	   clk,
-	input	   nRst,
-   input    sclk,    // serial clock
-   input    in,      // serial data in
-   input    sel,     // serial input listening when high also resets watch dog
-   output   woof     // The counter has lapsed when high
+	input	         clk,
+	input	         nRst,
+   input [31:0]   time_ns,
+   input          pet,
+   output reg     woof
 );
 
-   parameter DEFAULT = 32'd1000;
+   parameter CLK_HZ  = 32'd50000000;
 
-   reg   [31:0]   top;
-   reg   [31:0]   count;
+   reg   petted;
+   wire  hit;
+   
+   timer timer(
+      .clk              (clk              ),
+      .nRst             (nRst             ),
+      .period_ns        (time_ns          ),
+      .clk_frequency_hz (CLK_HZ           ),
+      .hit              (hit              )
+   );
 
-   assign woof = (count) ? 1'b0 : 1'b1;
-
-   always @(posedge clk or negedge nRst) begin
-      if(!nRst | sel) begin
-         count <= top;
-      end else begin
-         if(count) begin
-            count <= count - 32'd1;
-         end
-      end
-   end
-
-   always @(posedge sclk or negedge nRst) begin
-      if(!nRst) begin
-         top <= DEFAULT;
-      end else begin
-         if(sel) begin
-            top <= {top[30:0],in};
-         end
-      end
-   end
+	always@(posedge clk or negedge nRst) begin
+		if(!nRst) begin
+	      woof <= 1'b0;
+		end else begin
+         if(hit) begin
+            if(petted) begin
+               woof     <= 1'b0;
+               petted   <= 1'b0;
+            end else begin
+               woof <= 1'b1;
+            end
+         end else begin
+            if(pet) begin
+               petted   <= 1'b1;
+               woof     <= 1'b0;
+            end
+         end 
+		end
+	end
 endmodule
--- watchdog/watchdog_tb.v
+++ watchdog/watchdog_tb.v
@@ -1,66 +1,58 @@
+`timescale 1ns/1ps
 module watchdog_tb;
 
-	parameter CLK_PERIOD = 20;
+	parameter CLK_PERIOD = 20;    // 50Mhz
 
-
-   integer i;
-
-   reg   clk;
-	reg   nRst;
-   reg   sclk;
-   reg   in;
-   reg   sel;
-   wire  woof;
+	reg            clk;
+	reg            nRst;
+   reg   [31:0]   time_ns;
+   reg            pet;
+   wire           woof;
 
 	watchdog watchdog(
-		.clk	   (clk),
-		.nRst	   (nRst),
-      .sclk    (sclk),
-      .in      (in),
-      .sel     (sel),
-      .woof    (woof)
+		`ifdef POST_SYNTHESIS
+			.clk	   (clk),
+			.nRst	   (nRst)
+		`else
+			.clk	   (clk     ),
+			.nRst	   (nRst    ),
+		   .time_ns (time_ns ),
+         .pet     (pet     ),
+         .woof    (woof    )
+      `endif
 	);
 
-	initial begin
+	initial begin          
 		while(1) begin
 			#(CLK_PERIOD/2) clk = 0;
 			#(CLK_PERIOD/2) clk = 1;
 		end	end
 
 	initial begin
-		$dumpfile("watchdog.vcd");
-		$dumpvars(0,watchdog_tb);
+		`ifdef POST_SYNTHESIS
+			$dumpfile("watchdog_syn.vcd");
+			$dumpvars(0,watchdog_tb);
+		`else
+			$dumpfile("watchdog.vcd");
+			$dumpvars(0,watchdog_tb);
+		`endif
+		$display("                  TIME    nRst");		$monitor("%tps       %d",$time,nRst);	end
+
+	initial begin
+					   pet      = 0;
+                  time_ns  = 32'd1000000; // 1ms
+                  nRst     = 1;
+		#100		   nRst     = 0;
+		#100		   nRst     = 1;
+      #500000     pet      = 0;
+		#1000000    pet      = 1;
+      #100        pet      = 0;
+      #1000000    pet      = 1;
+      #100        pet      = 0;
+      #2000000    pet      = 1;
+      #100        pet      = 0;
+      #100000
+		$finish;
 	end
-   
-   task clock_in_top;
-      input [31:0] top;
-      begin
-         sel = 1; 
-         for(i=31;i>=0;i=i-1) begin
-            #50   sclk  = 0;
-            #50   in    = top[i];
-            #50   sclk  = 1;
-         end
-         #50 sel = 0;
-      end
-   endtask
-   
-   
-   initial begin
-            nRst  = 1;
-            in    = 0;
-            sel   = 0;
-            sclk  = 1;
 
-      #100  nRst  = 0;
-      #100  nRst  = 1;
-
-      clock_in_top(32'd100);   
-      #30000
-      clock_in_top(32'd1234);   
-      #30000
-      clock_in_top(32'd1000);   
-      #30000
-      $finish;
-   end
 endmodule
