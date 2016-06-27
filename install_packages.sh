df7654029d73f2a9c10781e63d010ef10c0ea972
Ashley J. Robinson <ashley181291@gmail.com>
2016-06-27 14:35:11 +0100

    run install packages from fresh ubuntu install

--- install_packages.sh
+++ install_packages.sh
@@ -1,15 +1,23 @@
+sudo apt-get install git
+sudo apt-get install gawk
+sudo apt-get install clang
+sudo apt-get install tcl-dev
+sudo apt-get install libreadline-dev
+sudo apt-get install bison
+sudo apt-get install flex
+sudo apt-get install libftdi-dev
 sudo apt-get install verilog
 sudo apt-get install gtkwave
-sudo apt-get instal libftdi-dev
+sudo add-apt-repository ppa:saltmakrell/ppa
+sudo apt-get update
+sudo apt-get install yosys
 git clone https://github.com/cliffordwolf/icestorm.git icestorm
 cd icestorm
 make -j$(nproc)
 sudo make install
-igit clone https://github.com/cseed/arachne-pnr.git arachne-pnr
+cd ..
+git clone https://github.com/cseed/arachne-pnr.git arachne-pnr
 cd arachne-pnr
 make -j$(nproc)
 sudo make install
-git clone https://github.com/cliffordwolf/yosys.git yosys
-cd yosys
-make -j$(nproc)
-sudo make install
+cd ..
