a57c9097f0749f56c3a1b91c258d469488e8a638
Ashley J. Robinson <ashley181291@gmail.com>
2016-09-10 19:51:03 +0100

    default swicth checked in

---
+++ switch_test/switch_test.v
@@ -0,0 +1,34 @@
+`timescale 1ns/1ps
+module switch_test(
+	input				clk,
+	input				nRst,
+	input				rx,
+	input				sw2,
+	input				sw1,
+	input				sw0,
+	output	reg	tx,
+	output	reg	led4,
+	output	reg	led3,
+	output	reg	led2,
+	output	reg	led1,
+	output	reg	led0
+);
+
+	always@(posedge clk or negedge nRst) begin
+		if(!nRst) begin
+			tx   <= 1'b0;
+			led4 <= 1'b0;
+			led3 <= 1'b0;
+			led2 <= 1'b0;
+			led1 <= 1'b0;
+			led0 <= 1'b0;
+		end else begin
+			tx   <= rx;
+			led4 <= sw1;
+			led3 <= 1'b1;
+			led2 <= sw2;
+			led1 <= 1'b1;
+			led0 <= sw0;
+		end
+	end
+endmodule
---
+++ switch_test/switch_test_tb.v
@@ -0,0 +1,87 @@
+`timescale 1ns/1ps
+module switch_test_tb;
+
+	parameter CLK_PERIOD = 20;
+
+	reg	clk;
+	reg	nRst;
+	reg	rx;
+	reg	sw2;
+	reg	sw1;
+	reg	sw0;
+	wire	tx;
+	wire	led4;
+	wire	led3;
+	wire	led2;
+	wire	led1;
+	wire	led0;
+
+	switch_test switch_test(
+		`ifdef POST_SYNTHESIS
+			.clk	(clk),
+			.nRst	(nRst),
+			.rx	(rx),
+			.sw2	(sw2),
+			.sw1	(sw1),
+			.sw0	(sw0),
+			.tx	(tx),
+			.led4	(led4),
+			.led3	(led3),
+			.led2	(led2),
+			.led1	(led1),
+			.led0	(led0)
+		`else
+			.clk	(clk),
+			.nRst	(nRst),
+			.rx	(rx),
+			.sw2	(sw2),
+			.sw1	(sw1),
+			.sw0	(sw0),
+			.tx	(tx),
+			.led4	(led4),
+			.led3	(led3),
+			.led2	(led2),
+			.led1	(led1),
+			.led0	(led0)
+		`endif
+	);
+
+	initial begin
+		while(1) begin
+			#(CLK_PERIOD/2) clk = 0;
+			#(CLK_PERIOD/2) clk = 1;
+		end
+	end
+
+	initial begin
+		`ifdef POST_SYNTHESIS
+			$dumpfile("switch_test_syn.vcd");
+			$dumpvars(0,switch_test_tb);
+		`else
+			$dumpfile("switch_test.vcd");
+			$dumpvars(0,switch_test_tb);
+		`endif
+		$display("                  TIME    nRst");		$monitor("%tps       %d",$time,nRst);
+	end
+
+	initial begin
+					nRst		= 1;
+					rx			= 0;
+					sw2		= 0;
+					sw1		= 0;
+					sw0		= 0;
+		#17		nRst		= 0;
+		#17		nRst		= 1;
+		#17		sw0		= 1;
+		#17		sw1		= 1;
+		#17		sw2		= 1;
+		#17		rx			= 1;
+		#17		sw1		= 0;
+		#17		sw2		= 0;
+		#17		sw0		= 0;
+		#17		rx			= 0;
+		#10
+		$finish;
+	end
+
+endmodule
