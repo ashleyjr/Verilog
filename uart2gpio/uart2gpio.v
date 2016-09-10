778771fa2765e87a1c1f9165e68844795b36d05f
Ashley J. Robinson <ashley181291@gmail.com>
2016-09-11 00:11:37 +0100

    started state machine for gpios

--- uart2gpio/uart2gpio.v
+++ uart2gpio/uart2gpio.v
@@ -9,32 +9,84 @@ module uart2gpio(
 	output   tx,
 	output   [4:0] led
 );
-   wire        bounce;
+   parameter   WAIT        = 8'd0;
+   parameter   READ        = 8'd1;
+   parameter   READ_BANK   = 8'd2;
+   parameter   READ_PIN    = 8'd3;
+   parameter   WRITE       = 8'd4;
+   parameter   WRITE_BANK  = 8'd5;
+   parameter   WRITE_PIN   = 8'd6;
+   
+   reg   [3:0] state;
+   reg   [7:0] bank;
+   reg   [7:0] pin;
+   reg   [7:0] data_tx;
+   reg         transmit;
+   
+   wire        recieved;
    wire [7:0]  data_rx;
-   wire [7:0]  data_tx;
-
-   assign led[0] = data_rx[0];
-   assign led[1] = data_rx[1];
-   assign led[2] = data_rx[2];
-   assign led[3] = data_rx[3];
-   assign led[4] = data_rx[4];
-
-   assign data_tx = data_rx;
-   //assign data_tx[0] = sw0;
-   //assign data_tx[1] = sw0;
-   //assign data_tx[2] = sw0;
-
+      
+   assign led[0] =   state[0]; 
+   assign led[1] =   state[1];
+   assign led[2] =   state[2];
+   assign led[3] =   state[3];
+   assign led[4] = 1'b0; 
+   
    uart_autobaud uart_autobaud(
       .clk           (clk        ),
       .nRst          (nRst       ),
-      .transmit      (bounce   ),
+      .transmit      (transmit),
       .data_tx       (data_tx),
       .rx            (rx         ),
       .busy_rx       (  ),
       .busy_tx       (  ),
-      .recieved      (bounce  ),
+      .recieved      (recieved  ),
       .data_rx       (data_rx    ),
-      .tx            (tx         )
+      .tx            (tx)
    );
+   
+   always@(posedge clk or negedge nRst) begin
+      if(!nRst) begin
+         state <= WAIT;
+      end else begin
+         if(recieved) begin
+            transmit <= 1'b1;
+            data_tx <= data_rx;
+            case(state)
+               WAIT:       case(data_rx)
+                              8'd48:   state <= READ;
+                              8'd49:   state <= WRITE;
+                           endcase
+               READ:       begin
+                              state <= READ_BANK;
+                              bank  <= data_rx; 
+                           end
+               READ_BANK:  begin
+                              state <= READ_PIN;
+                              pin   <= data_rx;
+                           end
+               READ_PIN:   begin
+                              state <= WAIT;
+                              data_tx <= 8'd50;
+                           end
+               WRITE:      begin
+                              state <= WRITE_BANK;
+                              bank <= data_rx;
+                           end
+               WRITE_BANK: begin
+                              state <= WRITE_PIN;
+                              pin <= data_rx;
+                           end
+               WRITE_PIN:  begin
+                              state <= WAIT;
+                              data_tx <= 8'd51;
+                           end
+            endcase
+         end else begin
+            transmit <= 1'b0;
+         end
+      end
+   end
+
 
 endmodule
