24bd80113e925f54d4d5c14cef368e1f4520144f
Ashley J. Robinson <ashley@ajrobinson.org>
2017-12-23 16:39:07 +0000

    Added state machine to async mem interface

--- up2_mem/up2_mem.v
+++ up2_mem/up2_mem.v
@@ -1,34 +1,88 @@
 `timescale 1ns/1ps
-module up2_mem(
-	input				clk,
-	input				nRst,
-	input				rx,
-	input				sw2,
-	input				sw1,
-	input				sw0,
-	output	reg	tx,
-	output	reg	led4,
-	output	reg	led3,
-	output	reg	led2,
-	output	reg	led1,
-	output	reg	led0
+module up2_mem#(
+    parameter                       addr_nibbles,
+    parameter                       data_nibbles,
+    parameter                       shift_width = 4*(addr_nibbles+data_nibbles),
+    parameter                       addr_width = (4*addr_nibbles),
+    parameter                       data_width = (4*data_nibbles)
+)(
+	input				            clk,
+	input				            nRst,
+
+    // Mem side
+    input                           i_read_ack,
+    output                          o_read_req,
+
+    input                           i_write_ack,
+    output                          o_write_req,
+    
+    output  [data_width-1:0]        i_data,
+    output  [addr_width-1:0]        o_addr,
+    output  [data_width-1:0]        o_data
+
+    // Regs side
+    input                           i_shift,
+    input   [3:0]                   i_shift_data,
+    output  [3:0]                   o_shift_data,
+    input                           i_swap_req,
+    output                          o_swap_ack,
 );
+
+    parameter   ONE
+    parameter   TWO
+    parameter   THREE
+
+    reg [shift_width-1:0] shift;
+    reg state
+    wire    [data_width-1]  shift_update;
+
+    assign  o_data          =   shift[data_width-1:0] ^ i_data;
+    assign  o_addr          =   shift[shift_width-1:data_width];
+    assign  o_shift         =   shift[3:0];
+    assign  shift_update    =   o_data ^ i_data;
 
 	always@(posedge clk or negedge nRst) begin
 		if(!nRst) begin
-			tx   <= 1'b0;
-			led4 <= 1'b0;
-			led3 <= 1'b0;
-			led2 <= 1'b0;
-			led1 <= 1'b0;
-			led0 <= 1'b0;
-		end else begin
-			tx   <= rx;
-			led4 <= sw1;
-			led3 <= 1'b1;
-			led2 <= sw2;
-			led1 <= 1'b1;
-			led0 <= sw0;
+		    shift       <= 'd0;
+        end else begin
+            o_read_req  <= 1'b0;
+            o_write_req <= 1'b0;
+            case(state)
+                IDLE:       begin
+                                if(i_shift) begin
+                                    shift       <= {i_shift,shift[shift_width-1:4]};
+                                end 
+                                if(i_swap) begin
+                                    o_read_req  <= 1'b1;
+                                    state       <= READ_REQ_1;
+                                end
+                            end
+                READ_REQ_1: begin
+                                if(i_read_ack) begin
+                                    shift[data_width-1:0]   <= shift_update;
+                                    o_write_req             <= 1'b1;
+                                    state                   <= WRITE_REQ
+                                end else begin
+                                    o_read_req <= 1'b1
+                                end
+                            end
+                WRITE_REQ:  begin
+                                if(i_write_ack) begin
+                                    o_read_req              <= 1'b1;
+                                    state                   <= READ_REQ_2;
+                                end else begin
+                                    o_write_req             <= 1'b1;
+                                end
+                            end
+                READ_REQ_2: begin
+                                if(i_read_ack) begin
+                                    shift[data_width-1:0]   <= shift_update; 
+                                    state                   <= IDLE
+                                end else begin
+                                    o_read_req              <= 1'b1;
+                                end
+                            end
+            endcase
 		end
 	end
 endmodule
