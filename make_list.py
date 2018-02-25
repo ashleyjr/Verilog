93b6fbd491017dbc3906a731dd4b45df57eba771
Ashley J. Robinson <ashley181291@gmail.com>
2018-02-25 12:34:58 +0000

    update list

--- make_list.py
+++ make_list.py
@@ -4,7 +4,8 @@ from os import listdir
 from os.path import isfile, join
 
 start = os.path.dirname(os.path.realpath(__file__))
-
+ignores =   [   ".vcd",
+                ".pdf"  ]
 lst = open("list.txt", "wb")
 
 for d in listdir(start):
@@ -13,8 +14,12 @@ for d in listdir(start):
         os.chdir(move)
         new = os.path.dirname(os.path.realpath(__file__))
         for f in listdir(new):
-            lst.write(os.path.join(d,f) + "\n")
-
+            add_to_list = True
+            for ignore in ignores:
+                if ignore in f:
+                    add_to_list = False
+            if add_to_list:
+                lst.write(os.path.join(d,f) + "\n")
 lst.close()
 
 
