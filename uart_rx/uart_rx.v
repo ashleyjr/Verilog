5965dfb1c3abcb7aa8365a09840f7915a92d2f1c
Ashley J. Robinson <ashley181291@gmail.com>
2018-03-01 22:06:57 +0000

    opt rx

--- uart_rx/uart_rx.v
+++ uart_rx/uart_rx.v
@@ -17,7 +17,7 @@ module uart_rx(
 	input    wire		      i_nrst,
    output   reg   [7:0]    o_data,
    input    wire           i_rx,
-	output   reg            o_valid
+	output   wire           o_valid
 );
 
     parameter  SAMPLE      = 1,  
@@ -30,14 +30,15 @@ module uart_rx(
    reg [1:0]                   state;
    reg [$clog2(SAMPLE)-1:0]    count;
 
-   assign full_sample = (count == SAMPLE);
-   assign half_sample = (count == (SAMPLE >> 1));
-
+   assign   full_sample =  (count == SAMPLE        );
+   assign   half_sample =  (count == (SAMPLE >> 1) );
+   assign   o_valid     =  (state == SM_IDLE       )  ?  1'b1  :
+                           (state == SM_WAIT       )  ?  1'b1  :
+                                                         1'b0  ;
    always@(posedge i_clk or negedge i_nrst) begin
       if(!i_nrst) begin
          state    <= SM_IDLE;	
-         count    <= 'b0;
-         o_valid  <= 1'b0;
+         count    <= 'b0; 
          o_data   <= 8'h0;
       end else begin
          count <= count + 'b1;
@@ -45,7 +46,6 @@ module uart_rx(
             SM_IDLE:       if(!i_rx) begin
                               state       <= SM_RX_START;
                               count       <= 'b0;
-                              o_valid     <= 1'b0;
                               o_data      <= START_BIT;
                            end
             SM_RX_START:   if(half_sample) begin
@@ -57,7 +57,6 @@ module uart_rx(
                               count       <= 'b0;
                               if(o_data[0]) begin
                                  state    <= SM_WAIT;
-                                 o_valid  <= 1'b1;
                               end
                            end
             SM_WAIT:       if(full_sample) begin
