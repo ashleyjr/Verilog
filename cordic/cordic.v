3048ee461df29507569c3e040038c19c23044c36
Ashley Robinson <ashley181291@gmail.com>
2018-09-18 18:04:59 +0100

    moved lut to wire

--- cordic/cordic.v
+++ cordic/cordic.v
@@ -21,10 +21,10 @@ module cordic(
                            x_shift,
                            y_shift,
                            sin,
-                           cos;
+                           cos,
+                           a;
    reg            [4:0]    count;
-   reg   signed   [19:0]   a,
-                           beta,
+   reg   signed   [19:0]  beta,
                            x,
                            y; 
  
@@ -50,7 +50,26 @@ module cordic(
    // 0.5 + 0.0625 + 0.03125 + 0.015625 = 0.609                               
    assign   sin      = (y >>> 1) + (y >>> 4) + (y >>> 5) + (y >>> 6);
    assign   cos      = (x >>> 1) + (x >>> 4) + (x >>> 5) + (x >>> 6);
-   
+  
+   // LUT
+   assign   a  =  (count == 'h0)	?  20'h0c90f   :	// 0.785398163397	 = arctan(2^-0)	= arctan(1.0)
+		            (count == 'h1)	?  20'h076b1   :	// 0.463647609001	 = arctan(2^-1)	= arctan(0.5)
+		            (count == 'h2)	?  20'h03eb6   :	// 0.244978663127	 = arctan(2^-2)	= arctan(0.25)
+		            (count == 'h3)	?  20'h01fd5   :	// 0.124354994547	 = arctan(2^-3)	= arctan(0.125)
+		            (count == 'h4)	?  20'h00ffa   :	// 0.062418809996	 = arctan(2^-4)	= arctan(0.0625)
+		            (count == 'h5)	?  20'h007ff   :	// 0.031239833430	 = arctan(2^-5)	= arctan(0.03125)
+		            (count == 'h6)	?  20'h003ff   :	// 0.015623728620	 = arctan(2^-6)	= arctan(0.015625)
+		            (count == 'h7)	?  20'h001ff   :	// 0.007812341060	 = arctan(2^-7)	= arctan(0.0078125)
+		            (count == 'h8)	?  20'h000ff   :	// 0.003906230132	 = arctan(2^-8)	= arctan(0.00390625)
+		            (count == 'h9)	?  20'h0007f   :	// 0.001953122516	 = arctan(2^-9)	= arctan(0.001953125)
+		            (count == 'ha)	?  20'h0003f   :	// 0.000976562190	 = arctan(2^-10)	= arctan(0.0009765625)
+		            (count == 'hb)	?  20'h0001f   :	// 0.000488281211	 = arctan(2^-11)	= arctan(0.00048828125)
+		            (count == 'hc)	?  20'h0000f   :	// 0.000244140620	 = arctan(2^-12)	= arctan(0.000244140625)
+		            (count == 'hd)	?  20'h00007   :	// 0.000122070312	 = arctan(2^-13)	= arctan(0.0001220703125)
+		            (count == 'he)	?  20'h00003   :	// 0.000061035156	 = arctan(2^-14)	= arctan(6.103515625e-05)
+		            //count == 'hf
+                                    20'h00001   ;	// 0.000030517578	 = arctan(2^-15)	= arctan(3.0517578125e-05)
+                           
    always@(posedge i_clk or negedge i_nrst) begin
 		if(!i_nrst | !i_req) begin 
          beta  <= 'd0;
@@ -70,25 +89,4 @@ module cordic(
          end
       end
 	end
-
-   always @(count) begin
-	   case(count)
-		   'h0:	a = 20'h0c90f;	// 0.785398163397	 = arctan(2^-0)	= arctan(1.0)
-		   'h1:	a = 20'h076b1;	// 0.463647609001	 = arctan(2^-1)	= arctan(0.5)
-		   'h2:	a = 20'h03eb6;	// 0.244978663127	 = arctan(2^-2)	= arctan(0.25)
-		   'h3:	a = 20'h01fd5;	// 0.124354994547	 = arctan(2^-3)	= arctan(0.125)
-		   'h4:	a = 20'h00ffa;	// 0.062418809996	 = arctan(2^-4)	= arctan(0.0625)
-		   'h5:	a = 20'h007ff;	// 0.031239833430	 = arctan(2^-5)	= arctan(0.03125)
-		   'h6:	a = 20'h003ff;	// 0.015623728620	 = arctan(2^-6)	= arctan(0.015625)
-		   'h7:	a = 20'h001ff;	// 0.007812341060	 = arctan(2^-7)	= arctan(0.0078125)
-		   'h8:	a = 20'h000ff;	// 0.003906230132	 = arctan(2^-8)	= arctan(0.00390625)
-		   'h9:	a = 20'h0007f;	// 0.001953122516	 = arctan(2^-9)	= arctan(0.001953125)
-		   'ha:	a = 20'h0003f;	// 0.000976562190	 = arctan(2^-10)	= arctan(0.0009765625)
-		   'hb:	a = 20'h0001f;	// 0.000488281211	 = arctan(2^-11)	= arctan(0.00048828125)
-		   'hc:	a = 20'h0000f;	// 0.000244140620	 = arctan(2^-12)	= arctan(0.000244140625)
-		   'hd:	a = 20'h00007;	// 0.000122070312	 = arctan(2^-13)	= arctan(0.0001220703125)
-		   'he:	a = 20'h00003;	// 0.000061035156	 = arctan(2^-14)	= arctan(6.103515625e-05)
-		   'hf:	a = 20'h00001;	// 0.000030517578	 = arctan(2^-15)	= arctan(3.0517578125e-05)
-	   endcase
-   end
 endmodule
