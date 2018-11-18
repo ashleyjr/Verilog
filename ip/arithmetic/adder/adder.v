19dee397b7ad059881ba9dd1ed3be95c60b40c7c
Ashley Robinson <ashley181291@gmail.com>
2018-11-18 15:12:40 +0000

    Added signs to adder

--- ip/arithmetic/adder/adder.v
+++ ip/arithmetic/adder/adder.v
@@ -1,17 +1,17 @@
 `timescale 1ns/1ps
 module adder(
-	input    wire  [DATA_WIDTH-1:0]  i_a,
-	input	   wire  [DATA_WIDTH-1:0]  i_b,
-	output   wire  [DATA_WIDTH-1:0]  o_q,
-   output   wire                    o_ovf
+	input    wire  signed [DATA_WIDTH-1:0]    i_a,
+	input	   wire  signed [DATA_WIDTH-1:0]    i_b,
+	output   wire  signed [DATA_WIDTH-1:0]    o_q,
+   output   wire                             o_ovf
 );
 
    parameter   DATA_WIDTH = 0;
 
-   wire  [DATA_WIDTH:0] q;
+   wire signed [DATA_WIDTH:0] q;
 
    assign q       = i_a + i_b;   
-   assign o_q     = q[DATA_WIDTH-1:0];
-   assign o_ovf   = q[DATA_WIDTH];
+   assign o_q     = {q[DATA_WIDTH],q[DATA_WIDTH-2:0]};
+   assign o_ovf   = q[DATA_WIDTH] ^ q[DATA_WIDTH-1];
 
 endmodule
--- ip/arithmetic/adder/adder_tb.v
+++ ip/arithmetic/adder/adder_tb.v
@@ -5,9 +5,9 @@ module adder_tb;
    parameter   DATA_WIDTH = 32;
 	
    
-   reg   [DATA_WIDTH-1:0]  i_a;
-	reg   [DATA_WIDTH-1:0]  i_b;
-	wire  [DATA_WIDTH-1:0]  o_q;
+   reg   signed [DATA_WIDTH-1:0]  i_a;
+	reg   signed [DATA_WIDTH-1:0]  i_b;
+	wire  signed [DATA_WIDTH-1:0]  o_q;
    wire                    o_ovf;
 
    adder #(
@@ -24,23 +24,74 @@ module adder_tb;
 		$dumpvars(0,adder_tb);
 	end
 
-	initial begin 
+   reg signed [DATA_WIDTH:0]  test;
+   reg signed [DATA_WIDTH:0]  top;
+   reg signed [DATA_WIDTH:0]  bot;  
+
+   initial begin
+      top = 32'h7FFFFFFF; 
+      bot = -32'h80000000;
+      while(1) begin
+         @(i_a or i_b);
+         #2 
+         test = i_a + i_b;
+         if((test > top) || (bot > test)) begin 
+            if(o_ovf != 1'b1) begin
+               $display("Overflow error");
+               #1
+               $finish;
+            end
+         end else begin
+            if(o_q != test) begin
+               $display("Add error");
+               #1
+               $finish;
+            end
+         end
+      end
+   end
+
+   initial begin 
 	   i_a   = 32'h0;
       i_b   = 32'h0;
       
       #5
-      i_a   = 32'hFFFFFFFF;
+      i_a   = 'd5;
+      i_b   = -'d1;
+      
+      #5
+      i_a   = -'d5;
+      i_b   = -'d1;
+
+      #5
+      i_a   = 32'h7FFFFFFF;
       i_b   = 32'h00000001;
       
       #5
-      i_a   = 32'h00000001;
-      i_b   = 32'h00000001;
+      i_a   = 32'h7FFFFFFF;
+      i_b   = -32'h7FFFFFFF;
       
       #5
       i_a   = 32'h80000000;
       i_b   = 32'h80000000;
       
-
+      #5
+      i_a   = -32'h7FFFFFFF;
+      i_b   = -32'h00000001;
+      
+      #5
+      i_a   = -32'h7FFFFFFE;
+      i_b   = -32'h00000001;
+      
+      #5
+      i_a   = -32'h80000000;
+      i_b   = -32'h00000001;
+      
+      repeat(1000) begin
+         #5
+         i_a = $urandom;
+         i_b = $urandom; 
+      end
       #5
      	$finish;
 	end
