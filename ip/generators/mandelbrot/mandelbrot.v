197a3ba1297a3417b9d9e1917a1628676fe18100
Ashley J. Robinson <ashley181291@gmail.com>
2019-02-24 21:08:04 +0000

    Test vectors

--- ip/generators/mandelbrot/mandelbrot.v
+++ ip/generators/mandelbrot/mandelbrot.v
@@ -1,34 +1,57 @@
 `timescale 1ns/1ps
 module mandelbrot(
-	input				i_clk,
-	input				i_nrst,
-	input				i_rx,
-	input				i_sw2,
-	input				i_sw1,
-	input				i_sw0,
-	output	reg	o_tx,
-	output	reg	o_led4,
-	output	reg	o_led3,
-	output	reg	o_led2,
-	output	reg	o_led1,
-	output	reg	o_led0
-);
+	input    wire		                        i_clk,
+	input	   wire		                        i_nrst,
+	input	   wire	signed   [WIDTH-1:0]       i_c_re,
+	input	   wire  signed   [WIDTH-1:0]		   i_c_im,
+	input    wire				                  i_valid,
+	output   reg            [WIDTH_ITERS-1:0] o_iter,
+	output	wire	                           o_done
+); 
+   parameter   WIDTH       = 16;
+   parameter   ITERS       = 256;
+   parameter   WIDTH_ITERS = $clog2(ITERS);
 
-	always@(posedge i_clk or negedge i_nrst) begin
-		if(!i_nrst) begin
-			o_tx   <= 1'b0;
-			o_led4 <= 1'b0;
-			o_led3 <= 1'b0;
-			o_led2 <= 1'b0;
-			o_led1 <= 1'b0;
-			o_led0 <= 1'b0;
-		end else begin
-			o_tx   <= i_rx;
-			o_led4 <= i_sw1;
-			o_led3 <= 1'b1;
-			o_led2 <= i_sw2;
-			o_led1 <= 1'b1;
-			o_led0 <= i_sw0;
-		end
+   wire           [WIDTH_ITERS-1:0] iter_next;
+   wire                             reset;
+   reg   signed   [WIDTH-1:0]       re;
+   wire  signed   [WIDTH-1:0]       re_sq;
+   wire  signed   [WIDTH-1:0]       re_next;
+   reg   signed   [WIDTH-1:0]       im;
+   wire  signed   [WIDTH-1:0]       im_sq;
+   wire  signed   [WIDTH-1:0]       im_next;
+   wire  signed   [WIDTH-1:0]       abs;
+
+   assign reset      = o_done | ~i_valid;
+   
+   assign re_sq      = re*re;
+   assign im_sq      = im*im;
+   
+   assign re_next    = (reset) ? 'd0 : (re_sq) - (im_sq) + i_c_re;   
+   assign im_next    = (reset) ? 'd0 : ((im*re) << 1) + i_c_im;
+
+   assign abs        = re_sq + im_sq;
+   assign unbounded  = (abs > (2 ** (WIDTH-1)));
+
+   always@(posedge i_clk or negedge i_nrst) begin
+      if(!i_nrst) begin
+         re <= 'd0;
+         im <= 'd0;
+      end else begin
+         re <= re_next;
+         im <= im_next;
+      end
+	end 
+
+   assign iter_next  =  (o_done)    ?  'd0:
+                        (i_valid)   ?  o_iter + 'd1:
+                                       'd0;
+
+   always@(posedge i_clk or negedge i_nrst) begin
+		if(!i_nrst)    o_iter <= 'd0;
+      else           o_iter <= iter_next;	
 	end
+   
+   assign o_done  = unbounded | (o_iter == (ITERS-1));
+
 endmodule
--- ip/generators/mandelbrot/mandelbrot_tb.v
+++ ip/generators/mandelbrot/mandelbrot_tb.v
@@ -1,67 +1,76 @@
 `timescale 1ns/1ps
 module mandelbrot_tb;
 
-	parameter CLK_PERIOD = 20;
+	parameter   CLK_PERIOD  = 20;
+   parameter   WIDTH       = 16;
+   parameter   ITERS       = 256;
+	
+   reg	                     i_clk;
+	reg	                     i_nrst;
+	reg   [WIDTH-1:0]          i_c_re;
+	reg   [WIDTH-1:0]          i_c_im;
+	reg	                     i_valid;
+	wire	[$clog2(ITERS)-1:0]  o_iter;
+	wire                       o_done;	
 
-	reg	clk;
-	reg	nrst;
-	reg	rx;
-	reg	sw2;
-	reg	sw1;
-	reg	sw0;
-	wire	tx;
-	wire	led4;
-	wire	led3;
-	wire	led2;
-	wire	led1;
-	wire	led0;
-
-	mandelbrot mandelbrot(
-		.i_clk	(clk),
-		.i_nrst	(nrst),
-		.i_rx		(rx),
-		.i_sw2	(sw2),
-		.i_sw1	(sw1),
-		.i_sw0	(sw0),
-		.o_tx		(tx),
-		.o_led4	(led4),
-		.o_led3	(led3),
-		.o_led2	(led2),
-		.o_led1	(led1),
-		.o_led0	(led0)
+	mandelbrot #(
+      .WIDTH   (WIDTH   ),
+      .ITERS   (ITERS   )
+   ) mandelbrot (
+		.i_clk	(i_clk   ),
+		.i_nrst	(i_nrst  ),
+		.i_c_re  (i_c_re  ),
+		.i_c_im	(i_c_im  ),
+		.i_valid	(i_valid ),
+		.o_iter	(o_iter  ),
+		.o_done  (o_done  )
 	);
 
 	initial begin
 		while(1) begin
-			#(CLK_PERIOD/2) clk = 0;
-			#(CLK_PERIOD/2) clk = 1;
+			#(CLK_PERIOD/2) i_clk = 0;
+			#(CLK_PERIOD/2) i_clk = 1;
 		end
 	end
 
 	initial begin
-			$dumpfile("mandelbrot.vcd");
-			$dumpvars(0,mandelbrot_tb);
-		$display("                  TIME    nrst");		$monitor("%tps       %d",$time,nrst);
+		$dumpfile("mandelbrot.vcd");
+		$dumpvars(0,mandelbrot_tb);
+	   $display("                  TIME    nrst");		
+      $monitor("%tps       %d",$time,i_nrst);
 	end
 
-	initial begin
-					nrst		= 1;
-					rx			= 0;
-					sw2		= 0;
-					sw1		= 0;
-					sw0		= 0;
-		#17		nrst		= 0;
-		#17		nrst		= 1;
-		#17		sw0		= 1;
-		#17		sw1		= 1;
-		#17		sw2		= 1;
-		#17		rx			= 1;
-		#17		sw1		= 0;
-		#17		sw2		= 0;
-		#17		sw0		= 0;
-		#17		rx			= 0;
-		#10
+   initial begin
+      #100000
 		$finish;
+	end
+
+   task compute;
+      input [WIDTH-1:0] re;
+      input [WIDTH-1:0] im;
+      begin
+         i_valid  = 1;
+         i_c_re   = re;
+         i_c_im   = im;
+         while(0 == o_done) begin 
+            @(posedge i_clk);
+         end
+         i_valid   = 0;
+      end
+   endtask
+	
+   initial begin
+               i_c_re   = 0;
+               i_c_im   = 0;
+               i_valid  = 0;
+					i_nrst   = 1;	
+		#17		i_nrst	= 0;
+      #17      i_nrst   = 1;
+		
+      #1000    compute(1,1);
+      #1000    compute(0,0);
+      #1000    compute(1,1);
+
 	end
 
 endmodule
